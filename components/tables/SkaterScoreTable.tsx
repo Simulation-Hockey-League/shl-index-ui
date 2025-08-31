6330f4a92cfc55b61ab7ab700b93a1afdb5bd213
luketd <lukedamato99@gmail.com>
2025-08-30 21:17:59 -0400

    Hyperlinks only added in player page

--- components/tables/SkaterScoreTable.tsx
+++ components/tables/SkaterScoreTable.tsx
@@ -91,20 +91,27 @@ export const SkaterScoreTable = ({
                   header: () => <TableHeader title="Team">Team</TableHeader>,
                   cell: (props) => {
                     const cellValue = props.getValue();
+                    if (type === 'player') {
+                      return (
+                        <Link
+                          href={{
+                            pathname: '/[league]/team/[id]',
+                            query: {
+                              league: router.query.league,
+                              id: props.row.original.teamID,
+                              season: props.row.original.season,
+                            },
+                          }}
+                          className="inline-block w-full max-w-[100px] truncate text-center leading-none text-blue600"
+                        >
+                          {cellValue}
+                        </Link>
+                      );
+                    }
                     return (
-                      <Link
-                        href={{
-                          pathname: '/[league]/team/[id]',
-                          query: {
-                            league: router.query.league,
-                            id: props.row.original.teamID,
-                            season: props.row.original.season,
-                          },
-                        }}
-                        className="inline-block w-full max-w-[100px] truncate text-center leading-none text-blue600"
-                      >
+                      <span className="inline-block w-full max-w-[100px] truncate text-center leading-none">
                         {cellValue}
-                      </Link>
+                      </span>
                     );
                   },
                 }),
