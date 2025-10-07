e574e62d89fe189804ccba189a27f99e4801aeb1
luketd <lukedamato99@gmail.com>
2025-10-06 21:15:53 -0400

    Update index.ts

--- pages/api/v2/teams/index.ts
+++ pages/api/v2/teams/index.ts
@@ -51,27 +51,45 @@ export default async (
 
   const leagues = Array.isArray(league) ? league : [league];
 
-  const [season] =
-    (!Number.isNaN(+seasonid) && [{ SeasonID: +seasonid }]) ||
-    (await query(
-      SQL`SELECT DISTINCT SeasonID
-    FROM team_data
-    WHERE LeagueID IN (`.append(leagues.join(',')).append(SQL`)
-    ORDER BY SeasonID DESC
-    LIMIT 1
-    `),
-    ));
-
-  const search = SQL`
-    SELECT t.TeamID, t.LeagueID, t.SeasonID, t.Name, t.Nickname, t.Abbr, t.PrimaryColor, t.SecondaryColor, t.TextColor, t.ConferenceID, t.DivisionID, s.Wins, s.Losses, s.OTL, s.SOW, s.SOL, s.Points, s.GF, s.GA, s.PCT
-    FROM team_data AS t
+  let search;
+  if (!Number.isNaN(+seasonid)) {
+    search = SQL`
+      SELECT t.TeamID, t.LeagueID, t.SeasonID, t.Name, t.Nickname, t.Abbr, t.PrimaryColor, t.SecondaryColor, t.TextColor, t.ConferenceID, t.DivisionID, 
+             s.Wins, s.Losses, s.OTL, s.SOW, s.SOL, s.Points, s.GF, s.GA, s.PCT
+      FROM team_data AS t
       INNER JOIN team_records AS s
         ON t.TeamID = s.TeamID
         AND t.SeasonID = s.SeasonID
         AND t.LeagueID = s.LeagueID
-    WHERE t.LeagueID IN (`.append(leagues.join(',')).append(SQL`)
-      AND t.SeasonID=${season.SeasonID}
-  `);
+      WHERE t.LeagueID IN (`.append(leagues.join(',')).append(SQL`)
+        AND t.SeasonID=${+seasonid}
+    `);
+  } else {
+    search = SQL`
+      SELECT t.TeamID, t.LeagueID, t.SeasonID, t.Name, t.Nickname, t.Abbr, t.PrimaryColor, t.SecondaryColor, t.TextColor, t.ConferenceID, t.DivisionID, 
+             s.Wins, s.Losses, s.OTL, s.SOW, s.SOL, s.Points, s.GF, s.GA, s.PCT
+      FROM team_data AS t
+      INNER JOIN team_records AS s
+        ON t.TeamID = s.TeamID
+        AND t.SeasonID = s.SeasonID
+        AND t.LeagueID = s.LeagueID
+      INNER JOIN (
+        SELECT LeagueID, TeamID, MAX(SeasonID) AS LatestSeason
+        FROM team_data
+        WHERE LeagueID IN (`
+      .append(leagues.join(','))
+      .append(
+        SQL`)
+        GROUP BY LeagueID, TeamID
+      ) latest
+        ON t.TeamID = latest.TeamID
+        AND t.SeasonID = latest.LatestSeason
+        AND t.LeagueID = latest.LeagueID
+      WHERE t.LeagueID IN (`,
+      )
+      .append(leagues.join(',')).append(SQL`)
+    `);
+  }
 
   if (!Number.isNaN(+conference)) {
     search.append(SQL` AND ConferenceID=${+conference}`);
@@ -88,7 +106,7 @@ export default async (
     season: team.SeasonID,
     league: team.LeagueID,
     conference: team.ConferenceID,
-    division: team.DivisionID === -1 ? undefined : team.DivisionID, // If there is no division dont include it in our data
+    division: team.DivisionID === -1 ? undefined : team.DivisionID,
     name: `${team.Name} ${team.Nickname}`,
     abbreviation: team.Abbr,
     location:
